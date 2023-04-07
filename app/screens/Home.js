c49ffa3f970cd60f62ae93a291de615af81483a3
Satyam Koshta <satyamkoshta340@gmail.com>
2023-04-07 16:03:30 +0530

    created newFileButton component

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,93 +1,17 @@
-import { StyleSheet, View, Image, Pressable, NativeModules } from 'react-native'
+import { StyleSheet, View, Image, Pressable } from 'react-native'
 // import { RNCamera } from 'react-native-camera'
-import { REACT_APP_URL } from  "@env";
-import React, { useContext, useState } from 'react'
-import { Modal, Portal, Text, Button, TextInput, Snackbar } from 'react-native-paper';
-import axios from "axios"
-import AuthContext from '../store/authContext';
+import React, { useState } from 'react'
+import { Text, Snackbar } from 'react-native-paper';
+import NewFileButton from '../components/NewFileButton';
 
 const Home = ({navigation}) => {
-  const authCtx = useContext(AuthContext);
-  const [visibleNewFileModel, setVisibleNewFileModel] = useState(false);
   const [snackbarText, setSnackbarText] = useState("")
   const [snackbarVisibility, setSnackbarVisibility] = useState(false);
-  const [fileName, setFileName] = useState(null);
-  const [description, setDescription] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
 
-  const showNewFileModal = () => setVisibleNewFileModel(true);
-  const hideNewFileModal = () => setVisibleNewFileModel(false);
-  const createNewFile = async () =>{
-    if( !fileName || ! description){
-      setSnackbarText("Fill the details correctly")
-      setSnackbarVisibility(true)
-      return
-    }
-    setIsLoading(true)
-    const body = { fileName: fileName, description: description}
-    const resp =  await axios.post( `${REACT_APP_URL}/api/file`, body, {
-      method: 'POST',
-      headers:{
-        'content-type': 'application/json',
-        'Authorization': `Bearer ${authCtx.token}`
-      }
-    })
-    
-    const response= resp.data;
-
-    if( response.status === "success"){
-      setFileName(null);
-      setDescription(null);
-      setSnackbarText("Fill created Successfully!")
-      setSnackbarVisibility(true)
-    }
-    
-    setIsLoading(false)
-  }
   return (
     <View style={styles.wraper}>
-      <Portal>
-        <Modal visible={visibleNewFileModel} onDismiss={hideNewFileModal} contentContainerStyle={styles.model}>
-          <Text variant='titleLarge' style={{paddingBottom: 30}}>Create New File</Text>
-          <TextInput
-            label="File Name"
-            mode="outlined"
-            style={{marginBottom: 20}}
-            onChangeText={ text => setFileName(text)}
-            value={fileName}
-          />
-          <TextInput
-            label="Description"
-            style={{marginBottom: 20, height: 80}}
-            multiline={true}
-            onChangeText={ text => setDescription(text)}
-            value={description}
-          />
-          <Button
-            mode="contained-tonal"
-            style={{marginBottom: 20}}
-            onPress={() => createNewFile()}
-            loading={isLoading}
-            disabled={isLoading}
-          >
-            Create
-          </Button>
-        </Modal>
-      </Portal>
       <View style={styles.row}>
-        <Pressable style={({ pressed }) => [
-        {
-          transform: [{
-            scale: pressed ? 1.07 : 1
-          }],
-          backgroundColor: '#2277ee'
-        },
-        styles.card, styles.rowCard]}
-        onPress={()=>showNewFileModal()}
-        >
-            <Image source={require('../assets/add_files.png')} style={{ width: 100, height: 100}}/>
-            <Text style={styles.cardText}> New File</Text>
-        </Pressable>
+        <NewFileButton setSnackbarText={setSnackbarText} setSnackbarVisibility={setSnackbarVisibility}/>
         <Pressable style={({ pressed }) => [
         {
           transform: [{
@@ -123,11 +47,13 @@ const Home = ({navigation}) => {
             <Text style={styles.cardText}> Send Out File </Text>
         </Pressable>
         <View style={{alignItems:"center", flex: 1}}>
+        </View>
+        <View>
         <Snackbar
           visible={snackbarVisibility}
           onDismiss={() => setSnackbarVisibility(false)}
           action={{
-            label: 'Close'
+              label: 'Close'
           }}>
           {snackbarText}
         </Snackbar>
@@ -168,12 +94,5 @@ const styles = StyleSheet.create({
         height: 100,
         marginTop: 20
     },
-    model:{
-      // flex:0.5,
-      height: 400,
-      backgroundColor: 'white',
-      padding: 20,
-      marginHorizontal: 20,
-      borderRadius: 10
-    }
+    
 })
